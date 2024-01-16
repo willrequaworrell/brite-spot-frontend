a47f3ac0b6e5e1989f74086741755154d3bce730
Will <willrequaworrell@gmail.com>
2024-01-15 22:29:27 -0500

    signup form now working with no validation, login next

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react"
 import { useForm } from "react-hook-form";
+import CustomButton from "./CustomButton";
+
+import { createUser} from "../util/apiCalls";
 
 const Auth = () => {
-	const { register, handleSubmit, watch, formState: { errors } } = useForm();
-	const onSubmit = data => console.log(data);
+	const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
 	const [authModeToggle, setAuthModeToggle] = useState("Sign Up")
+
+	const onSubmit = (data) => {
+		console.log(data)
+		createUser(data)
+		reset({ 
+            email: "",   
+            password: "",
+            first_name: ""
+        });
+	}
 
 	const toggleAuthMode = () => {
 		if (authModeToggle === "Sign Up") {
@@ -14,40 +26,51 @@ const Auth = () => {
 		}
 	}
 
-	console.log(watch("email"))
-	console.log(watch("password"))
+	// console.log(watch("email"))
+	// console.log(watch("password"))
+
 
 	return (
 		<div className="h-full w-full flex-col justify-center items-center">
 			<div className="flex justify-center items-center">
-				<img src="/logo2.png" alt="logo" className="h-64 -mt-8 -mr-12" />
-				<h1 className="text-8xl ">BriteSpot</h1>
+				<img src="/logo2.png" alt="logo" className="h-32  -mr-4" />
+				<h1 className="text-6xl ">BriteSpot</h1>
 			</div>
 			<div className="flex justify-center">
-				<div className="w-[35%] h-[28rem] flex justify-center rounded-xl bg-black p-1 shadow-xl">
+				<div className="w-[35%]  flex justify-center rounded-xl bg-black p-1 shadow-xl">
 					<div className="h-full w-full rounded-xl bg-white p-4">
-						<div className="flex justify-center m-2">
-							<h2 className="text-2xl">{authModeToggle}</h2>
+						<div className="flex-col items-center text-center m-2">
+							<h2 className="text-2xl m-2">{authModeToggle}</h2>
+							<div className="h-1 w-full bg-gradient-to-r from-teal-500 via-yellow-500 to-pink-500"></div>
 						</div>
 						<form onSubmit={handleSubmit(onSubmit)} className="flex-col items-center w-full">
 							{/* register your input into the hook by invoking the "register" function */}
 							<div className="">
+								{authModeToggle === "Sign Up" && (
+									<div className="m-4 flex-col text-center">
+									<p className="">First Name</p>
+									<input {...register("first_name", { required: true })} className="p-1 border-2 border-black rounded-lg"/>
+								</div>
+								)}
 								<div className="m-4 flex-col text-center">
 									<p className="">Email</p>
 									<input {...register("email", { required: true })} className="p-1 border-2 border-black rounded-lg"/>
 								</div>
 								<div className="m-4 text-center">
 									<p>Password</p>
-									<input {...register("password", { required: true })} className="p-1 border-2 border-black rounded-lg" />
+									<input {...register("password", { required: true })} type="password" className="p-1 border-2 border-black rounded-lg" />
 								</div>
 
 							</div>
 							{/* errors will return when field validation fails  */}
 							{errors.exampleRequired && <span>This field is required</span>}
-							<div className="flex justify-center ">
-								<input type="submit" className="cursor-pointer" />
+							<div className="flex justify-center">
+								<div className="w-1/4 text-center border-4 border-black rounded-lg hover:scale-105 hover:bg-gradient-to-r from-teal-500 via-yellow-500 to-pink-500">
+									<input type="submit" className="cursor-pointer" />
+								</div>
 
 							</div>
+							
 							
 						</form>
 						<div className="flex justify-center m-2" onClick={toggleAuthMode}>
--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -2,10 +2,19 @@ import axios from "axios"
 
 const apiKeyHeader = {"x-api-key": import.meta.env.VITE_API_KEY}
 
+export const createUser = async (userData) => {
+    try {
+        console.log("submitting", userData)
+        const response = await axios.post(`http://localhost:3000/signup`, userData , {headers: apiKeyHeader} )
+        console.log(response)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
 export const fetchAllUserEntries = async (userId) => {
     try {
-        
-        
         const response = await axios.get(`http://localhost:3000/user/${userId}/entries`, {headers: apiKeyHeader})
         // console.log(response.data.entries)
         return response.data.entries
