74579faf247f2d8c3ac8e45b4a04202b7384c936
Will <willrequaworrell@gmail.com>
2024-02-08 23:58:04 -0500

    working on state to make entry container only appear when user hasn't posted today

---
+++ src/Components/AlreadyPostedMessage.jsx
@@ -0,0 +1,10 @@
+
+const AlreadyPostedMessage = () => {
+  return (
+    <div>
+        <p>{"Looks like you've already made an entry today. Come back tomorrow to make another one or delete today's from your history"}</p>
+    </div>
+  )
+}
+
+export default AlreadyPostedMessage
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,47 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import EntryContainer from "./EntryContainer"
 import Navbar from "./Navbar"
+import { useAuth } from "../Context/AuthContext"
+import { fetchMostRecentUserEntry, submitEntry } from "../util/apiCalls"
+import AlreadyPostedMessage from "./AlreadyPostedMessage"
 
 
 
 const Home = () => {
+  const {currentUser} = useAuth()
+  const [isLoading, setIsLoading] = useState(true)
+  const [hasPostedToday, setHasPostedToday] = useState(true)
+
+  const isToday = (dateString) => {
+    const date = new Date(dateString);
+    const today = new Date();
+    return date.getDate() === today.getDate() &&
+          date.getMonth() === today.getMonth() &&
+          date.getFullYear() === today.getFullYear();
+  }
+
+  useEffect(() => {
+    const fetchWrapper = async () => {
+      try {
+        const response = await fetchMostRecentUserEntry(currentUser)
+        const postIsToday = isToday(response.date)
+        setHasPostedToday(postIsToday)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchWrapper().then(setIsLoading(false))
+    
+  } , [currentUser])
 
   return (
     <div className="h-screen w-full bg-gray-100">
         <Navbar/>
-        <div className="flex justify-center items-center">
-            <EntryContainer/>
-        </div>
+        {!isLoading && 
+          <div className="flex justify-center items-center">
+              {hasPostedToday ? <AlreadyPostedMessage /> : <EntryContainer/> }
+          </div>
+        }
     </div>
   )
 }
