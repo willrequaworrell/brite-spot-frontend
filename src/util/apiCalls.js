59c72bd18762c98a0fb5289d6202292a8da2491d
Will <willrequaworrell@gmail.com>
2024-03-10 20:54:17 -0400

    changed env variables to allow deploy to prod

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -6,7 +6,7 @@ const apiKeyHeader = {"x-api-key": import.meta.env.VITE_API_KEY}
 export const createUser = async (userData) => {
     try {
         console.log("submitting", userData)
-        const response = await axios.post(`http://localhost:3000/signup`, userData , {headers: apiKeyHeader} )
+        const response = await axios.post(`http://${import.meta.env.VITE_DOMAIN}:3000/signup`, userData , {headers: apiKeyHeader} )
         console.log(response)
         return {status: "success", userData: response}
     } catch (e) {
@@ -22,7 +22,7 @@ export const signInUser = async (credentials) => {
     }
     try {
         console.log("signing in", editedCredentials)
-        const response = await axios.post(`http://localhost:3000/login`, editedCredentials , {headers: apiKeyHeader} )
+        const response = await axios.post(`http://${import.meta.env.VITE_DOMAIN}:3000/login`, editedCredentials , {headers: apiKeyHeader} )
         // console.log(response)
         setTokenCookie(response.data.jwt)
         return {status: "success", userData: response.data.jwt}
@@ -33,7 +33,7 @@ export const signInUser = async (credentials) => {
 
 export const fetchUserDetails = async (userId) => {
     try {
-        const response = await axios.get(`http://localhost:3000/user/${userId}`, {headers: apiKeyHeader})
+        const response = await axios.get(`http://${import.meta.env.VITE_DOMAIN}:3000/user/${userId}`, {headers: apiKeyHeader})
         return response.data
     } catch (error) {
         console.log(error)
@@ -42,7 +42,7 @@ export const fetchUserDetails = async (userId) => {
 
 export const fetchAllUserEntries = async (userId) => {
     try {
-        const response = await axios.get(`http://localhost:3000/user/${userId}/entries`, {headers: apiKeyHeader})
+        const response = await axios.get(`http://${import.meta.env.VITE_DOMAIN}:3000/user/${userId}/entries`, {headers: apiKeyHeader})
         if (response.data.status === "none") {
             return null
         }
@@ -55,7 +55,7 @@ export const fetchAllUserEntries = async (userId) => {
 
 export const fetchMostRecentUserEntry = async (userId) => {
     try {
-        const response = await axios.get(`http://localhost:3000/user/${userId}/entries/today`, {headers: apiKeyHeader})
+        const response = await axios.get(`http://${import.meta.env.VITE_DOMAIN}:3000/user/${userId}/entries/today`, {headers: apiKeyHeader})
         if (response.data.status === "none") {
             return null
         }
@@ -67,7 +67,7 @@ export const fetchMostRecentUserEntry = async (userId) => {
 
 export const fetchWordCloudData = async (userId) => {
     try {
-        const response = await axios.get(`http://localhost:3000/user/${userId}/entries/combined`, {headers: apiKeyHeader})
+        const response = await axios.get(`http://${import.meta.env.VITE_DOMAIN}:3000/user/${userId}/entries/combined`, {headers: apiKeyHeader})
         return response.data.wordCountsArray
     }
     catch (e) {
@@ -77,7 +77,7 @@ export const fetchWordCloudData = async (userId) => {
 
 export const submitEntry = async (entryData) => {
     try {
-        const response = await axios.post('http://localhost:3000/entry', entryData, {headers: apiKeyHeader} )
+        const response = await axios.post(`http://${import.meta.env.VITE_DOMAIN}:3000/entry`, entryData, {headers: apiKeyHeader} )
         return {status: "success", message: "Entry logged successfully"};
     } catch (e) {
         return {status: "error", errorData: e};
@@ -86,7 +86,7 @@ export const submitEntry = async (entryData) => {
 
 export const deleteEntry = async (entryId) => {
     try {
-        const response = await axios.delete(`http://localhost:3000/entry/${entryId}`, {headers: apiKeyHeader});
+        const response = await axios.delete(`http://${import.meta.env.VITE_DOMAIN}:3000/entry/${entryId}`, {headers: apiKeyHeader});
         console.log(response);
         return {status: "success", message: "Entry deleted successfully"};
     } catch (e) {
