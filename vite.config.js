7b82c99e064516712701116bcc847a562d489ff4
Will <willrequaworrell@gmail.com>
2024-03-11 22:23:00 -0400

    more

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // define: {
-  //   __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  // },
+  define: {
+    "process.env": process.env,
+    VITE_DOMAIN: process.env.VITE_DOMAIN,
+  },
 })
