25eb24dc05395605aa9731b344e047c088df5e11
Will <willrequaworrell@gmail.com>
2024-03-22 19:20:21 -0400

    removed old debugging comments

--- src/util/cookieHelper.js
+++ src/util/cookieHelper.js
@@ -1,11 +1,9 @@
 import Cookies from 'js-cookie';
 
 export const setTokenCookie = (token) => {
-    // console.log("cookie")
     Cookies.set('jwt', token);
 };
 
 export const removeTokenCookie = () => {
-  // Remove the token cookie
   Cookies.remove('jwt');
 };
